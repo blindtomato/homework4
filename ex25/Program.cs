583fd8f74044264345c77d21adafd060c0654a4a
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-04 19:43:18 +0700

    feat: add solution of task 25

---
+++ ex25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Pow(int baseOfDegree, int indicator)
+{
+    int result = 1;
+    for(int i = 1; i<= indicator; i++)
+        result = result * baseOfDegree;
+    return result;
+}
+
+Console.WriteLine("Введите число, которое будет использоваться в качестве основания степени");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите натуральное число, которое будет использоваться в качестве показателя степени");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if (num2 <= 0)
+    Console.WriteLine("Вы ввели некорректное число");
+else
+    Console.WriteLine($"{num1} в степени {num2} равно {Pow(num1, num2)}.");
