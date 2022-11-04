f93dad5d5ba46b2485fd2a643b6afe3a29b5e4ae
Yudin Pavel <yudin.p.s@gmail.com>
2022-11-04 20:41:10 +0700

    feat: add solution of task 29

---
+++ ex29/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+int[] fillArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for(int i = 0; i < lenght; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Convert.ToInt32 (Console.ReadLine());
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+printArray(fillArray(8));
